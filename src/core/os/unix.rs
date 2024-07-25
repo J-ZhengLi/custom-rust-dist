f4c28c7da699f74bcbeb21428fe0f7417c75649a
J-ZhengLi <lizheng135@huawei.com>
2024-07-25 14:12:05 +0800

    implemented `install` and `uninstall` for unix systems

--- src/core/os/unix.rs
+++ src/core/os/unix.rs
@@ -1,3 +1,5 @@
+use std::{path::PathBuf, sync::OnceLock};
+
 use crate::{
     core::{
         cargo_config::CargoConfig, InstallConfiguration, Installation, UninstallConfiguration,
@@ -5,15 +7,38 @@ use crate::{
     },
     utils,
 };
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
+
+/// Indicate whether if the [`Installation::init`] was called.
+///
+/// If calling `.get()` on this lock returns a `None`, meaning it hasn't been
+/// initialized, which leads to a conclusion that `init` was indeed not called.
+static INIT_ONCE: OnceLock<()> = OnceLock::new();
 
 impl Installation for InstallConfiguration {
+    fn init(&self) -> Result<()> {
+        // Create a new folder to hold installation
+        let folder = &self.install_dir;
+        utils::mkdirs(folder)?;
+
+        // Create a copy of this binary to install dir
+        let self_exe = std::env::current_exe()?;
+        let cargo_bin_dir = self.cargo_home().join("bin");
+        utils::mkdirs(&cargo_bin_dir)?;
+        utils::copy_to(&self_exe, &cargo_bin_dir)?;
+
+        INIT_ONCE.get_or_init(|| ());
+        Ok(())
+    }
+
     // On linux, persistent env vars needs to be written in `.profile`, `.bash_profile`, etc.
     // Rustup already did all the dirty work by writting an entry in those files
     // to invoke `$CARGO_HOME/env.{sh|fish}`. Sadly we'll have to re-implement a similar procedure here,
     // because rustup will not write those file if a user has choose to pass `--no-modify-path`.
     // Which is not ideal for env vars such as `RUSTUP_DIST_SERVER`.
     fn config_rustup_env_vars(&self) -> Result<()> {
+        ensure_init_call();
+
         let vars_raw = self.env_vars()?;
         for sh in shell::get_available_shells() {
             // Shell commands to set env var, such as `export KEY='val'`
@@ -45,6 +70,8 @@ impl Installation for InstallConfiguration {
     }
 
     fn config_cargo(&self) -> Result<()> {
+        ensure_init_call();
+
         let mut config = CargoConfig::new();
         if let Some((name, url)) = &self.cargo_registry {
             config.add_source(name, url.to_owned(), true);
@@ -52,7 +79,11 @@ impl Installation for InstallConfiguration {
 
         let config_toml = config.to_toml()?;
         if !config_toml.trim().is_empty() {
-            let config_path = self.cargo_home().join("config.toml");
+            // make sure cargo_home dir exists
+            let cargo_home = self.cargo_home();
+            utils::mkdirs(&cargo_home)?;
+
+            let config_path = cargo_home.join("config.toml");
             utils::write_file(config_path, &config_toml, false)?;
         }
 
@@ -69,7 +100,7 @@ impl Uninstallation for UninstallConfiguration {
                 let new_content = remove_sub_string_between(
                     content,
                     shell::RC_FILE_SECTION_START,
-                    &format!("{}\n", shell::RC_FILE_SECTION_END)
+                    shell::RC_FILE_SECTION_END
                 ).ok_or_else(
                     || anyhow!(
                         "unable to remove rustup config section from shell profile: '{}'. \
@@ -84,13 +115,74 @@ impl Uninstallation for UninstallConfiguration {
         }
         Ok(())
     }
+
+    fn remove_self(&self) -> Result<()> {
+        // TODO: Run `rustup self uninstall` first
+        // TODO: Remove possibly installed extensions for other software, such as `vs-code` plugins.
+
+        // Remove the installer dir.
+        let installed_dir = install_dir_from_exe_path()?;
+        std::fs::remove_dir_all(installed_dir)?;
+        Ok(())
+    }
 }
 
-fn remove_sub_string_between(mut input: String, start: &str, end: &str) -> Option<String> {
-    let start_pos = input.find(start)?;
-    let end_pos = input.find(end)? + end.len();
-    input.drain(start_pos..=end_pos);
-    Some(input)
+/// Try getting the installation root judging be current executable path.
+//
+// This program should be installed under `{install_dir}/.cargo/bin/`,
+// we should be able to track the installation dir by going up three parents.
+// We should also make sure it is indeed the installation dir by checking if
+// the folder fits the characteristic.
+// FIXME: There might be risks involved, resulting unintended directory being removed
+// after uninstallation.
+fn install_dir_from_exe_path() -> Result<PathBuf> {
+    let exe_path = std::env::current_exe().context("cannot locate current executable")?;
+    let comp_count = exe_path.components().count();
+    let maybe_install_dir: PathBuf = exe_path
+        .components()
+        .take(comp_count.saturating_sub(3))
+        .collect();
+
+    if !maybe_install_dir.is_dir() {
+        // Check if it exists, this could fail if comp_count was less then `3`,
+        // meaning the current exe was put into root dir, or any folder that are not deep enough.
+        bail!(
+            "install directory does not exist, \
+        make sure this binary is in its original location before running uninstall."
+        );
+    }
+    if !maybe_install_dir.ends_with(env!("CARGO_PKG_NAME")) {
+        // Check if the install dir's name is correct. This could fail if someone has
+        // put this binary in an arbitrary folder, then run uninstallation, which
+        // resulted in removing unintended directories.
+        bail!(
+            "directory '{}' does not seems like the currect install root, \
+        make sure this binary is in its original location before running uninstall.",
+            maybe_install_dir.display()
+        );
+    }
+
+    Ok(maybe_install_dir)
+}
+
+fn remove_sub_string_between(input: String, start: &str, end: &str) -> Option<String> {
+    // TODO: this might not be an optimized solution.
+    let start_pos = input.lines().position(|line| line == start)?;
+    let end_pos = input.lines().position(|line| line == end)?;
+    let result = input
+        .lines()
+        .take(start_pos)
+        .chain(input.lines().skip(end_pos + 1))
+        .collect::<Vec<_>>()
+        .join("\n");
+    Some(result)
+}
+
+fn ensure_init_call() {
+    assert!(
+        INIT_ONCE.get().is_some(),
+        "Internal Error: `Installation::init` should be called first"
+    );
 }
 
 /// Unix shell module, contains methods that are dedicated in configuring rustup env vars.
@@ -131,7 +223,7 @@ mod shell {
             format!(
                 "{RC_FILE_SECTION_START}\n\
                 {raw_content}\n\
-                {RC_FILE_SECTION_END}\n"
+                {RC_FILE_SECTION_END}"
             )
         }
     }
@@ -277,6 +369,9 @@ mod shell {
 
 #[cfg(test)]
 mod tests {
+    use crate::utils;
+    use std::path::PathBuf;
+
     use super::shell;
 
     #[test]
@@ -312,8 +407,59 @@ export RUSTUP_UPDATE_ROOT='https://example.com/rustup'
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-. \"$HOME/.cargo/env\"
-"
+. \"$HOME/.cargo/env\""
         );
+    }
+
+    #[test]
+    fn labeled_section_at_the_end() {
+        let mocked_profile = r#"
+alias autoremove='sudo pacman -Rcns $(pacman -Qdtq)'
+. "$HOME/.cargo/env"
+
+# ===== rustup config section START =====
+export CARGO_HOME='/home/.cargo'
+export RUSTUP_HOME='/home/.rustup'
+# ===== rustup config section END ====="#;
+        let new = super::remove_sub_string_between(
+            mocked_profile.to_string(),
+            shell::RC_FILE_SECTION_START,
+            shell::RC_FILE_SECTION_END,
+        )
+        .unwrap();
+        assert_eq!(
+            new,
+            r#"
+alias autoremove='sudo pacman -Rcns $(pacman -Qdtq)'
+. "$HOME/.cargo/env"
+"#
+        );
+    }
+
+    // TODO: Move this test to `utils`
+    #[test]
+    fn path_ambiguity() {
+        let with_dots = PathBuf::from("/path/to/home/./my_app/../my_app");
+        let without_dots = PathBuf::from("/path/to/home/my_app");
+        assert_ne!(with_dots, without_dots);
+
+        let with_dots_comps: PathBuf = with_dots.components().collect();
+        let without_dots_comps: PathBuf = without_dots.components().collect();
+        // Components take `..` accountable in case of symlink.
+        assert_ne!(with_dots_comps, without_dots_comps);
+
+        let with_dots_normalized = utils::to_nomalized_abspath(&with_dots).unwrap();
+        let without_dots_normalized = utils::to_nomalized_abspath(&without_dots).unwrap();
+        assert_eq!(with_dots_normalized, without_dots_normalized);
+    }
+
+    #[test]
+    fn estimated_install_dir() {
+        let mocked_exe_path = PathBuf::from("/path/to/home/my_app/.cargo/bin/my_app");
+        let anc_count = mocked_exe_path.components().count();
+        // Components count root dir (/) as well, so there should be 8 components.
+        assert_eq!(anc_count, 8);
+        let maybe_install_dir: PathBuf = mocked_exe_path.components().take(anc_count - 3).collect();
+        assert_eq!(maybe_install_dir, PathBuf::from("/path/to/home/my_app"));
     }
 }
