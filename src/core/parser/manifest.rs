c8061dc184626897fdafd1f8c962b0fb863c7049
J-ZhengLi <lizheng135@huawei.com>
2024-08-08 00:26:33 +0800

    fix tests for windows

--- src/core/parser/manifest.rs
+++ src/core/parser/manifest.rs
@@ -265,7 +265,7 @@ t4 = { git = "https://git.example.com/org/tool", branch = "stable" }
             tools,
             BTreeMap::from([
                 (
-                    &"mingw".into(),
+                    &"mingw64".into(),
                     &ToolInfo::Path {
                         path: PathBuf::from(
                             "tests/cache/x86_64-13.2.0-release-posix-seh-msvcrt-rt_v11-rev1.7z"
