f4211380b2dbe6592d2cd632cef69f082f32166e
Jonathan Lee <lizheng135@huawei.com>
2024-08-13 15:49:47 +0800

    Revert "Use Vite + Vue instead (#31)"
    This reverts commit 7244f507f8c63cba97d855edb706411f9f473fcf.

--- installer/src/components/BaseInput.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <input
-    px="15px"
-    py="8px"
-    rounded="4px"
-    b="1px solid base hover:active focus:active"
-    outline-none
-  />
-</template>
--- installer/src/utils/types/Component.ts
+++
@@ -1,7 +0,0 @@
-export interface Component {
-  id: number;
-  name: string;
-  desc: string[];
-  required: boolean;
-  checked: boolean;
-}
