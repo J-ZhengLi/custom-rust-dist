4ea2eaf20b24bcd79503a64335b1edf60f10a76b
J-ZhengLi <lizheng135@huawei.com>
2024-10-11 10:35:13 +0800

    trigger rebuild when `locales` files changed

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 use std::env;
 
 const TARGET_OVERRIDE_ENV: &str = "TARGET_OVERRIDE";
+const FILES_TO_TRIGGER_REBUILD: &[&str] = &["locales/en.json", "locales/zh-CN.json"];
 
 fn main() {
     println!("cargo:rerun-if-env-changed={TARGET_OVERRIDE_ENV}");
+    for file in FILES_TO_TRIGGER_REBUILD {
+        println!("cargo:rerun-if-changed={file}");
+    }
 
     let target = env::var(TARGET_OVERRIDE_ENV).unwrap_or(env::var("TARGET").unwrap());
     println!("cargo:rustc-env=TARGET={target}");
