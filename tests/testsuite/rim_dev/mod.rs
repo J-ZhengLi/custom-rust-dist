54368b3975a5c85e5848acaa5d3bd4715ff7fabc
LuuuXXX <luuux98@163.com>
2024-12-20 16:54:57 +0800

    add more help test

--- tests/testsuite/rim_dev/mod.rs
+++ tests/testsuite/rim_dev/mod.rs
@@ -1,1 +1,4 @@
+mod dist_help;
 mod help;
+mod run_manager_help;
+mod vendor_help;
---
+++ tests/testsuite/rim_dev/vendor_help/mod.rs
@@ -0,0 +1,13 @@
+use rim_test_support::file;
+use rim_test_support::prelude::*;
+
+#[rim_test]
+fn case() {
+    snapbox::cmd::Command::rim_dev()
+        .arg("vendor")
+        .arg("--help")
+        .assert()
+        .success()
+        .stdout_eq(file!["stdout.log"])
+        .stderr_eq(file!["stderr.log"]);
+}
